af9bba80ed1e473cb12c8889a2a8b7c1f5719db8
stastnyjakub <jakub.stastny@koala42.com>
2025-10-29 17:46:46 +0100

    🚨 fix: unused imports

--- frontend/src/modules/users/components/UsersTablePage/UsersTablePage.tsx
+++ frontend/src/modules/users/components/UsersTablePage/UsersTablePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { UsersTable } from '../UsersTable/UsersTable';
 import type { User } from '../../../../types';
 import { getUsersList } from '../../../../api';
